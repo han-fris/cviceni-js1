83611376a9ad9fc54f3e508d0a6b5b87ed5b9a11
Han Frišonsová <hanafrisonsova@gmail.com>
2025-03-22 12:32:05 +0100

    prvni hodina JS a cviceni

---
+++ script.js
@@ -0,0 +1,75 @@
+/*
+document.body.innerHTML += 'Řekl: "Ahoj světe!"';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 42;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += '42';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 5 + 12;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 5 * 12;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 5 / 12;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 5 + 12 * 2;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 12 / 5;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 27 % 5;
+*/
+//document.body.innerHTML += '';
+//document.body.innerHTML += 0;
+//document.body.innerHTML += '0';
+/*
+document.body.innerHTML += Math.round(12.521);
+document.body.innerHTML += '<br>';
+document.body.innerHTML += Math.floor(12.65);
+document.body.innerHTML += '<br>';
+document.body.innerHTML += Math.ceil(12.6);
+document.body.innerHTML += '<br>';
+document.body.innerHTML += Math.floor(Math.random() * 6) + 1;
+*/
+
+/* CVIČENÍ
+document.body.innerHTML += 'Měsíční plat: ';
+document.body.innerHTML += 7 * 320 * 21 + 'Kč';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 'Daň: ';
+document.body.innerHTML += Math.floor(0.15 * (0.4 * (7 * 320 * 21))) + 'Kč';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 'Daň z proměnné mzdy: ';
+const plat = 40000;
+document.body.innerHTML += Math.floor(0.15 * (0.4 * plat)) + 'Kč';
+document.body.innerHTML += '<br>';
+
+document.body.innerHTML += '<br>';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 'Jak dlouhý je Pán prstenů?';
+document.body.innerHTML += '<br>';
+
+document.body.innerHTML += Math.floor(223 / 60) + ':';
+document.body.innerHTML += 223 % 60;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 'han' + '.' + 'frisonsova' + '@gmail.com';
+*/
+/*
+const cisloNaKostce = Math.floor(Math.random() * 6) + 1;
+document.body.innerHTML += 'Na kostce padlo ' + cisloNaKostce;
+*/
+/*
+document.body.innerHTML += '<br>';
+let sazba = 500;
+let vyplata = 8 * 21 * sazba;
+document.body.innerHTML += vyplata;
+
+document.body.innerHTML += '<br>';
+sazba = 600;
+vyplata = 8 * 21 * sazba;
+document.body.innerHTML += vyplata;
+*/
+/*
+//na toto pozor, podle toho jak napisu hodnoty tak to muze delat neco jineho napr 1213
+document.body.innerHTML += '<br>';
+document.body.innerHTML += '12' + 13;
+*/
